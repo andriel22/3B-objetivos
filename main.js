96efbfa8f03905ad7cf23e5a9c1ebf5882ca209b
Andriel <141413682+andriel22@users.noreply.github.com>
2025-05-05 08:35:05 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -6,17 +6,18 @@ const tempoObjetivo2 = new Date("2025-12-05T00:00:00");
 const tempoObjetivo3 = new Date("2025-12-05T00:00:00");
 const tempoObjetivo4 = new Date("2025-12-05T00:00:00");
 
-const tempos = [tempoObjetivo1, tempoObjetivo2 ,tempoObjetivo3 ,tempoObjetivo4];
- 
-for (let i=0; i<contadores.length;i++){
-    contadores[i].textContent = calculaTempo(tempos[i])
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
+function atualizaCronometro() {
+    for (let i = 0; i < contadores.length; i++) {
+        contadores[i].textContent = calculaTempo(tempos[i])
+    }
 }
 
 
-
-for(let i = 0; i< botoes.length; i++){
+for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
-        for(let j = 0; j< botoes.length; j++){
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
 
@@ -26,21 +27,27 @@ for(let i = 0; i< botoes.length; i++){
     }
 }
 
-function calculaTempo(tempoObjetivo){
-    
+function calculaTempo(tempoObjetivo) {
+
 
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
 
-    let segundos = Math.floor(tempoFinal/1000);
-    let minutos = Math.floor(segundos/60);
-    let horas = Math.floor(minutos/60);
-    let dias = Math.floor(horas/24);
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
 
-    segundos %=60;
-    minutos %=60;
-    horas %=24;
-
-    return dias  +  "Dias"  +  horas  +  "Horas"  +  minutos  +  "Minutos"  +  segundos  +  "Segundos";
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+    if (tempoFinal > 0) {
+        return dias + "Dias" + horas + "Horas" + minutos + "Minutos" + segundos + "Segundos";
+    } else {
+        return "Objetivo concluído"
+    }
 
 }
+
+atualizaCronometro();
+setInterval(atualizaCronometro, 1000);
